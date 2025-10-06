be049739854823c59a788a118699c63116e4b6f4
ancgroup <s.narita@anc-a.co.jp>
2025-10-06 18:44:42 +0900

    Fix Footer: remove wrong TopPage import

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,31 +1,12 @@
-// src/App.tsx
+// src/components/Footer.tsx
 import React from "react";
-import { Routes, Route } from "react-router-dom";
 
-import TopPage from "./pages/TopPage";
-import VehicleRegisterPage from "./pages/VehicleRegisterPage";
-import DriverRegisterPage from "./pages/DriverRegisterPage";
-import ReportNewPage from "./pages/ReportNewPage";
-import ReportEditPage from "./pages/ReportEditPage";
-import DailyReportListPage from "./pages/DailyReportListPage";
-import Footer from "./components/Footer"; // ← フッター追加
-
-const App: React.FC = () => {
+const Footer: React.FC = () => {
   return (
-    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-      <div style={{ flex: 1 }}>
-        <Routes>
-          <Route path="/" element={<TopPage />} />
-          <Route path="/vehicles" element={<VehicleRegisterPage />} />
-          <Route path="/drivers" element={<DriverRegisterPage />} />
-          <Route path="/report/new" element={<ReportNewPage />} />
-          <Route path="/report/edit/:id" element={<ReportEditPage />} />
-          <Route path="/reports" element={<DailyReportListPage />} />
-        </Routes>
-      </div>
-      <Footer /> {/* ← 全ページ共通表示 */}
-    </div>
+    <footer style={{ textAlign: "center", marginTop: "2rem", fontSize: "0.9rem", color: "#555" }}>
+      <p>Copyright © ANC co.,ltd. All Rights Reserved.</p>
+    </footer>
   );
 };
 
-export default App;
+export default Footer;
